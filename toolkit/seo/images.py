480e4122f3376508277d5ba88f95abab4646b3f0
StanGirard <girard.stanislas@gmail.com>
2020-06-20 21:49:23 +0200

    [IMAGES] Added duplicates and bugfix

--- toolkit/seo/images.py
+++ toolkit/seo/images.py
@@ -4,26 +4,25 @@ from toolkit.lib.http_tools import request_parse
 import urllib
 
 def find_all_images(url):
-    list_images = {"images":[], "summary":{"missing_title": 0, "missing_alt": 0, "total": 0}}
+    list_images = {"images":[], "summary":{"missing_title": 0, "missing_alt": 0,"duplicates":0, "total": 0}}
     list_urls =  []
     soup = request_parse(url, timeout=1)
     images = soup.findAll('img')
     for image in images:
         url = urllib.parse.urljoin(url,image['src'])
-        alt = None
-        title = None
+        alt = image.get("alt")
+        title = image.get("title")
         list_images["summary"]["total"] += 1
-        if "alt" in image:
-            alt  = image['alt']
-        else:
+    
+        if not alt:
             list_images["summary"]["missing_alt"] += 1
-        if "title" in image:
-            title = image["title"]
-        else:
+        if not title:
             list_images["summary"]["missing_title"] += 1
         if url not in list_urls:
             list_images["images"].append({"url": url, "alt": alt, "title": title})
             list_urls.append(url)
+        else:
+            list_images["summary"]["duplicates"] +=1
     return list_images
     
 

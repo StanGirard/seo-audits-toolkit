44111d3afced26d10c2b678e9522f46b41ecf9de
StanGirard <girard.stanislas@gmail.com>
2020-06-27 13:25:16 +0200

    [DASHBOARD] Graphs

--- toolkit/routes/dashboard.py
+++ toolkit/routes/dashboard.py
@@ -1,8 +1,10 @@
 from flask import current_app as app
 from flask import  render_template, request
 from toolkit import dbAlchemy
-from toolkit.models import Serp
+from toolkit.models import Serp, Graphs
 from toolkit.routes.serp import query_domain_serp
+from toolkit.routes.graphs import generate_interactive_graph
+import urllib
 
 @app.route('/')
 def home():
@@ -17,11 +19,27 @@ def rank_get():
         result = query_domain_serp( query, domain, "en", "com")
         if "limit" in result:
             error = result
-    print("hello")
     result = Serp.query.order_by(Serp.begin_date.desc()).all()
-    print("hello1")
     result_list = []
-    print(result)
     for i in result:
         result_list.append({"pos": i.pos, "url": i.pos, "query": i.query_text, "time": i.begin_date})
     return render_template("rank.jinja2", result=result_list, error=error)
+
+@app.route('/graphs', methods=["POST", "GET"])
+def graphs_get():
+    error = None
+    if request.method == "POST":
+        domain = request.form["domain"]
+        result = generate_interactive_graph(domain, False, 500)
+        if "error" in result:
+            error = result
+    results = Graphs.query.all()
+    result_arr=[]
+    for i in results:
+        result_arr.append({"id": i.id, "urls": i.urls, "status_job": i.status_job, "begin_date": i.begin_date})
+    return render_template("graphs_all.jinja2", result=result_arr)
+
+@app.route('/graphs/<id>', methods=["GET"])
+def graphs_get_by_id(id):
+    results = Graphs.query.filter(Graphs.id == id).first()
+    return render_template("bokeh.jinja2", script=results.script, div=results.div, domain=urllib.parse.urlparse(results.urls).netloc, template="Flask", time=results.begin_date)
--- toolkit/routes/graphs.py
+++ toolkit/routes/graphs.py
@@ -13,19 +13,6 @@ from sqlalchemy import update
 
 
 def update_or_insert_graph_in_db( urls, maximum, updating=False):
-    """Update or inserts html in the DB
-
-    Arguments:
-        conn {Connection} -- DB connector
-        urls {string} -- Root Domain
-        maximum {int} -- Maximum number of urls to crawl
-
-    Keyword Arguments:
-        update {bool} -- update or insert (default: {False})
-
-    Returns:
-        HTML Render -- Renders the Graph
-    """
     plot, domain = generate_graph_internal_link_interactive(urls, maximum)
     script, div = components(plot)
     conn = db.engine.connect()
@@ -83,5 +70,5 @@ def interactive_graph_all():
     results = Graphs.query.all()
     result_arr={"results":[]}
     for i in results:
-        result_arr["results"].append({"urls": i.urls, "status_job": i.status_job})
+        result_arr["results"].append({"urls": i.urls, "status_job": i.status_job, "begin_date": i.begin_date})
     return result_arr

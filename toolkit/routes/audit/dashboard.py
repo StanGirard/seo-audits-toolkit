6b554a6ff61b1539d7f14e9657e65e07455701dd
StanGirard <girard.stanislas@gmail.com>
2020-07-15 10:46:30 +0200

    [ROUTES] Templates folder added

--- toolkit/routes/audit/dashboard.py
+++ toolkit/routes/audit/dashboard.py
@@ -10,37 +10,12 @@ from toolkit import dbAlchemy as db
 from toolkit.controller.seo.lighthouse import audit_google_lighthouse_full
 from toolkit.models import Audit, LighthouseScore
 
-@app.template_filter('formatdatetime')
-def format_datetime(value, format="%d %b %Y %I:%M %p"):
-    """Format a date time to (Default): d Mon YYYY HH:MM P"""
-    if value is None:
-        return ""
-    return value.strftime(format)
+
 
 @app.route('/audit', methods=["GET"])
 def audit_home():
     return render_template("audit/audit.jinja2")
 
-@app.route('/audit/lighthouse/full')
-def dashboard_audit_lighthouse_full():
-    results = Audit.query.filter(Audit.type_audit == "Lighthouse_Full").all()
-    result_arr={"results": []}
-    for i in results:
-        result_arr["results"].append({"id": i.id, "url": i.url, "result": i.result, "begin_date": i.begin_date})
-    return result_arr
-
-@app.route('/audit/lighthouse/full', methods=["POST"])
-def add_audit_lighthouse_full():
-    url = request.form['url']
-    count = Audit.query.filter(Audit.url == url).filter(Audit.type_audit=="Lighthouse_Full").count()
-    if url and count == 0:
-        value = audit_google_lighthouse_full(url)
-        new_audit = Audit(
-            url = url, result=json.dumps(value), type_audit="Lighthouse_Full", begin_date=datetime.now()
-        )
-        db.session.add(new_audit)
-        db.session.commit()
-    return redirect(url_for('dashboard_audit_lighthouse_full'))
 
 @app.route('/audit/lighthouse/score', methods=["POST"])
 def add_audit_lighthouse_score():
---
+++ toolkit/routes/templates/lib.py
@@ -0,0 +1,9 @@
+from flask import current_app as app
+
+
+@app.template_filter('formatdatetime')
+def format_datetime(value, format="%d %b %Y %I:%M %p"):
+    """Format a date time to (Default): d Mon YYYY HH:MM P"""
+    if value is None:
+        return ""
+    return value.strftime(format)

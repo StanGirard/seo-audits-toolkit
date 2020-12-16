f108d699a72d541cba56c85dfe30530630fafb87
Luc Michalski <michalski.luc@gmail.com>
2020-12-16 06:28:24 +0100

    Added custom entrypoint with switch case, updated dockerfiles with tiny package as entrypoint

---
+++ server/docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -x
+set -e
+
+isArgPassed() {
+  arg="$1"
+  argWithEqualSign="$1="
+  shift
+  while [ $# -gt 0 ]; do
+    passedArg="$1"
+    shift
+    case $passedArg in
+    $arg)
+      return 0
+      ;;
+    $argWithEqualSign*)
+      return 0
+      ;;
+    esac
+  done
+  return 1
+}
+
+case "$1" in
+
+#  'runserver')
+#  	shift
+#  	exec python3 manage.py runserver $@
+# ;;
+
+  'bash')
+  	ARGS=""
+  	shift
+    apk add --update --no-cache nano bash jq 
+  	exec /bin/bash $@
+	;;
+
+  *)
+  	exec python3 manage.py runserver $@
+	;;
+esac

353fd6498cfc387f1324333e94693e41bc74e665
Stanislas Girard <stan@primates.dev>
2020-12-16 23:27:57 +0100

    [KEYWORDS][SERIALIZER] Changed settings

--- django/keywords/serializers.py
+++ django/keywords/serializers.py
@@ -5,32 +5,31 @@ from .tasks import keywords_job
 from .models import Keyword
 from django.utils import timezone
 import pytz
+import json
 
 class KeywordsSerializer(serializers.ModelSerializer):
-    language = serializers.CharField(write_only=True)
-    ngram = serializers.IntegerField(write_only=True)
-    number = serializers.IntegerField(write_only=True)
+    # language = serializers.CharField(write_only=True)
+    # ngram = serializers.IntegerField(write_only=True)
+    # number = serializers.IntegerField(write_only=True)
     class Meta:
         model = Keyword
-        fields = ['id', 'method', 'text', 'result','settings','language','ngram','number','status_job', 'task_id', 'last_updated' ]
+        fields = ['id', 'method', 'text', 'result','settings','status_job', 'task_id', 'last_updated' ]
         extra_kwargs = {
             'result': {'read_only': True},
             'status_job': {'read_only': True},
             'task_id': {'read_only': True},
             'last_updated': {'read_only': True},
-            'settings': {'read_only': True}
         }
     def create(self, validated_data):
         ## Creates the celery task
-        settings = {"language": validated_data["language"], "ngram": validated_data["ngram"], "number":validated_data["number"]}
-        keywords_task = keywords_job.delay(validated_data["text"],validated_data["language"],validated_data["ngram"],validated_data["number"])
+        keywords_task = keywords_job.delay(validated_data["text"],validated_data["settings"]["language"],validated_data["settings"]["ngram"],validated_data["settings"]["number"])
         
         ## Creates the Save to DB
         newKeyword = Keyword.objects.create(
         method=validated_data["method"],
         text=validated_data["text"],
         status_job="SCHEDULED",
-        settings=settings,
+        settings=validated_data["settings"],
         task_id=str(keywords_task.id), 
         result="", 
         last_updated=timezone.now()

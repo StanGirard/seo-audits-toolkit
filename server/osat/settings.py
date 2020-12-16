cead928dcc0a9e1ddfbdfa0651455c0f0e21477a
Luc Michalski <michalski.luc@gmail.com>
2020-12-16 16:12:55 +0100

    Fix typo

--- server/osat/settings.py
+++ server/osat/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en-us")
-TIME_ZONE = = os.environ.get("TIME_ZONE", "UTC")
+TIME_ZONE = os.environ.get("TIME_ZONE", "UTC")
 
 USE_I18N = True
 USE_L10N = True
